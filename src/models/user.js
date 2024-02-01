114fbfbd65fd0a8cb51a42805af39498c66989aa
hoang <hoangle1533@gmail.com>
2024-02-01 15:36:38 +0700

    #6-forgot and reset password

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
-const UserSchema = new mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        },
-        profilePic: {
-            type: String,
-            default: "",
-        },
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
     },
-    { timestamps: true }
-);
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+}, { timestamps: true });
+
+// UserSchema.methods.createResetPasswordToken = () => {
+//     const resetToken = crypto.randomBytes(32, this.toString('hex'));
+
+//     crypto.createHash('sha256').update(resetToken).digest('hex');
+// }
 
 module.exports = mongoose.model("User", UserSchema);
