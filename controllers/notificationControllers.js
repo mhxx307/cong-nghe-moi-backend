5bf1ef9ae20aba96611f8a4125b1b0267e97f801
mhxx307 <minhquan.lavo@gmail.com>
2024-04-10 11:44:26 +0700

    update

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -1,4 +1,4 @@
-const Notification = require('../models/Notification');
+const Notification = require('../models/notification');
 
 const notificationControllers = {
     sendNotification: async ({ senderId, receiverId, type, message }) => {
